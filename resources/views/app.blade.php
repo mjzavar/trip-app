0c71479dfe112607858ac3a3d1e6534478ac0853
mojtaba <mojtaba@teamupspace.com>
2024-06-03 17:30:04 +0330

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+<body>
+@inertia
+</body>
+</html>
